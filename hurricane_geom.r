779ce8f57373f21b1b84e88714c69226d497716e
mikemcclendon <mike.mcclendon@gmail.com>
2017-06-18 15:00:44 -0400

    fourth

--- hurricane_geom.r
+++ hurricane_geom.r
@@ -7,6 +7,7 @@ library(ggmap)
 library(stringr)
 library(geosphere)
 library(ggplot2)
+library(grid)
 
 #' make_ext_tracks
 #'
@@ -14,9 +15,12 @@ library(ggplot2)
 #'it into a format to use for visualization
 #'
 #' @return Returns a tidied data frame object
+#' 
+#' @importFrom readr, dplyr, lubridate
+#' 
 #' @export
 #'
-#' @examples
+#' @examples make_ext_tracks()
 
 make_ext_tracks <- function() {
   ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
@@ -49,48 +53,56 @@ make_ext_tracks <- function() {
 }
     
 
-#' Title
+#' get_storm_observation
+#' 
+#' Function to return a single tidied storm observation
 #'
-#' @param data 
-#' @param name 
-#' @param dt 
-#' @param tm 
+#' @param data ext_tracks data frame
+#' @param name name of storm passed as string
+#' @param dt date of observation 
+#' @param tm time of observation
 #'
-#' @return
+#' @return data frame containing observation of a single storm
+#' 
+#' @importFrom dplyr, lubridate
+#' 
 #' @export
 #'
-#' @examples
-get_storm_observation <- function(ext, name="IKE-2008", dt="2008-09-13", tm="12:00:00") {
+#' @examples get_storm_observation()
+
+get_storm_observation <- function(data = make_ext_tracks(), name="IKE-2008", dt="2008-09-13", tm="12:00:00") {
   dt <- ymd_hms(paste(dt, tm))
-  ext %>% 
+  data %>% 
     filter(storm_id == name & date == dt)}
 
+#' hurricane_proto
+#' 
+#' Constructs a hurricane ggproto object
+#'
+#' @return ggproto object
+#' 
+#' @importFrom ggplot2, grid 
+#' 
+#' @export
+#'
+#' @examples hurricane_proto
 
-
-geom_hurricane_proto <- ggplot2::ggproto("geom_hurricane_proto", Geom,
-                                         required_aes = c("x", "y",
-                                                          "r_ne", "r_se", "r_nw", "r_sw"
-                                         ),
+hurricane_proto <- ggproto("hurricane_proto", Geom, required_aes = c("x", "y",
+                                                          "r_ne", "r_se", "r_nw", "r_sw"),
                                          default_aes = aes(fill = 1, colour = 1, alpha = 1, scale_radii = 1),
                                          draw_key = draw_key_polygon,
                                          draw_group = function(data, panel_scales, coord) {
-                                           
-                                           ## Transform the data first
+                                           ## Transform data
                                            coords <- coord$transform(data, panel_scales)
-                                           
-                                           # Convert nautical miles to meters and multiply by scale factor
-                                           data <- data %>% mutate_(r_ne = ~r_ne*1852*scale_radii,
-                                                                    r_se = ~r_se*1852*scale_radii,
-                                                                    r_sw = ~r_sw*1852*scale_radii,
-                                                                    r_nw = ~r_nw*1852*scale_radii
+                                           nauticalConv <- 1852
+                                           data <- data %>% mutate_(r_ne = ~r_ne * nauticalConv * scale_radii,
+                                                                    r_se = ~r_se * nauticalConv * scale_radii,
+                                                                    r_sw = ~r_sw * nauticalConv * scale_radii,
+                                                                    r_nw = ~r_nw * nauticalConv * scale_radii
                                            )
-                                           
-                                           
-                                           # Loop over the data and create the points for each quandrant
+                                           # Create points for each quandrant
                                            for (i in 1:nrow(data)) {
-                                             
-                                             # Create the Northwest Quandrant
-                                             df_nw <- base::data.frame(colour = data[i,]$colour,
+                                             nw <- data.frame(colour = data[i,]$colour,
                                                                        fill = data[i,]$fill,
                                                                        geosphere::destPoint(p = c(data[i,]$x, data[i,]$y),
                                                                                             b = 270:360,
@@ -99,9 +111,7 @@ geom_hurricane_proto <- ggplot2::ggproto("geom_hurricane_proto", Geom,
                                                                        PANEL = data[i,]$PANEL,
                                                                        alpha = data[i,]$alpha
                                              )
-                                             
-                                             # Create the Northeast Quandrant
-                                             df_ne <- base::data.frame(colour = data[i,]$colour,
+                                             ne <- data.frame(colour = data[i,]$colour,
                                                                        fill = data[i,]$fill,
                                                                        geosphere::destPoint(p = c(data[i,]$x, data[i,]$y),
                                                                                             b = 1:90,
@@ -110,9 +120,7 @@ geom_hurricane_proto <- ggplot2::ggproto("geom_hurricane_proto", Geom,
                                                                        PANEL = data[i,]$PANEL,
                                                                        alpha = data[i,]$alpha
                                              )
-                                             
-                                             # Create the Southeast Quandrant
-                                             df_se <- base::data.frame(colour = data[i,]$colour,
+                                             se <- data.frame(colour = data[i,]$colour,
                                                                        fill = data[i,]$fill,
                                                                        geosphere::destPoint(p = c(data[i,]$x, data[i,]$y),
                                                                                             b = 90:180,
@@ -121,9 +129,7 @@ geom_hurricane_proto <- ggplot2::ggproto("geom_hurricane_proto", Geom,
                                                                        PANEL = data[i,]$PANEL,
                                                                        alpha = data[i,]$alpha
                                              )
-                                             
-                                             # Create the Southwest Quandrant
-                                             df_sw <- data.frame(colour = data[i,]$colour,
+                                             sw <- data.frame(colour = data[i,]$colour,
                                                                  fill = data[i,]$fill,
                                                                  geosphere::destPoint(p = c(data[i,]$x, data[i,]$y),
                                                                                       b = 180:270,
@@ -132,69 +138,68 @@ geom_hurricane_proto <- ggplot2::ggproto("geom_hurricane_proto", Geom,
                                                                  PANEL = data[i,]$PANEL,
                                                                  alpha = data[i,]$alpha
                                              )
-                                             
-                                             # bind all the rows into a dataframe
-                                             df_points <- dplyr::bind_rows(list(df_nw, df_ne, df_se, df_sw))
-                                             
+                                             points <- bind_rows(list(nw, ne, se, sw))
                                            }
-                                           
-                                           
-                                           # Rename columns x and y from lon and lat repectively
-                                           df_points <- df_points %>% dplyr::rename_('x' = 'lon',
-                                                                                     'y' = 'lat'
+                                           # Rename long and lat to x and y
+                                           points <- points %>% rename_('x' = 'lon', 'y' = 'lat'
                                            )
-                                           
-                                           # Convert to character
-                                           df_points$colour <- base::as.character(df_points$colour)
-                                           df_points$fill <- base::as.character(df_points$fill)
-                                           
-                                           
-                                           ## transform data points
-                                           coords_df <- coord$transform(df_points, panel_scales)
-                                           
+                                           # Correct for color read in
+                                           points$colour <- as.character(points$colour)
+                                           points$fill <- as.character(points$fill)
+                                           coords <- coord$transform(points, panel_scales)
                                            ## Construct grid polygon
-                                           grid::polygonGrob(
-                                             x= coords_df$x,
-                                             y = coords_df$y,
-                                             gp = grid::gpar(col = coords_df$colour, fill = coords_df$fill, alpha = coords_df$alpha)
+                                           polygonGrob(
+                                             x= coords$x,
+                                             y = coords$y,
+                                             gp = gpar(col = coords$colour, fill = coords$fill, 
+                                                             alpha = coords$alpha)
                                            )
-                                           
                                          }
                                          
 )
 
-
-
-#' Title
+#' geom_hurricane
+#' 
+#' A function to create a hurricane geom layer
 #'
-#' @param mapping 
-#' @param data 
-#' @param stat 
-#' @param position 
-#' @param na.rm 
-#' @param show.legend 
-#' @param inherit.aes 
+#' @param mapping aesthetic mappings
+#' @param data data to be displayed in this layer
+#' @param stat The statistical transformation to use on the data for this layer, as a string
+#' @param position Position adjustment, either as a string, or the result of a call to a position adjustment function
+#' @param show.legend logical. Should this layer be included in the legends?
+#' @param inherit.aes f FALSE, overrides the default aesthetics, rather than combining with them. 
 #' @param ... 
 #'
-#' @return
+#' @return ggproto layer
 #' @export
 #'
-#' @examples
+#' @examples geom_hurricane(data = storm_observation, aes(x = longitude, y = latitude) 
+  
 geom_hurricane <- function(mapping = NULL, data = NULL, stat = 'identity',
                            position = 'identity', na.rm = FALSE,
                            show.legend = NA, inherit.aes = TRUE, ...) {
-  ggplot2::layer(
-    geom = geom_hurricane_proto, mapping = mapping,
-    data = data, stat = stat, position = position, 
-    show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+  layer(geom = hurricane_proto, mapping = mapping,data = data, stat = stat, 
+        position = position,show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, ...)
     
   )
 }
 
+#' hurricane_plot
+#' 
+#' Returns plot of hurricane
+#'
+#' @return ggplot of hurricane
+#' 
+#' @importFrom ggmap, ggplot
+#' 
+#' @export
+#'
+#' @examples plot(hurricane_plot)
 
 
-map_ike <- get_map("Louisiana", zoom = 6, maptype = "toner-background") %>%
+#modified from provided code
+hurricane_plot <- get_map("Louisiana", zoom = 6, maptype = "toner-background") %>%
   ggmap(extent = "device") +
   geom_hurricane(data = storm_observation,
                  aes(x = longitude, y = latitude, 
